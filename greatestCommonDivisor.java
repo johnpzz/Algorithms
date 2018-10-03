9e8084d5866a63556a1860ef796e11699b3b1bd1
John Zubiri <34234157+johnpzz@users.noreply.github.com>
2018-10-03 16:06:44 -0700

    comment added, make sure to verify!

--- greatestCommonDivisor.java
+++ greatestCommonDivisor.java
@@ -16,6 +16,7 @@ public class greatestCommonDivisor {
     public static int gcd(int a, int b) {
         int divisor = 1;
         
+        // New note added: Here, I think we can go until Math.min(a,b)/2 because factors don't exceed half of the number.  (12 has 1,2,3,4,6)
         for (int i = 1; i <= Math.min(a, b); i++) {
             if (a%i == 0 && b%i == 0)
                 divisor = i;
